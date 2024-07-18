5be4c7c2eecda02973601b29ddfca954780d32e4
Sany_ranq <1005712028@qq.com>
2024-07-18 09:03:47 +0800

    flink 无界流

--- flink-demo/flink-01/src/main/java/org/example/StreamWordCountUnbounded.java
+++ flink-demo/flink-01/src/main/java/org/example/StreamWordCountUnbounded.java
@@ -4,18 +4,14 @@ import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-public class StreamWordCount {
+
+public class StreamWordCountUnbounded {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env =
                 StreamExecutionEnvironment.getExecutionEnvironment();
-        ParameterTool parameterTool = ParameterTool.fromArgs(args);
-//        String host = parameterTool.get("host");
-//        int port = parameterTool.getInt("port");
-//        String host = "hadoop102";
-//        int port = 7777;
-//        DataStream<String> inputDataStream = env.socketTextStream(host, port);
-        String inputPath = "D:\\workspace\\github\\hadoopDemo\\flink-demo\\flink-01\\src\\main\\resources\\input\\word.txt";
-        DataStream<String> inputDataStream = env.readTextFile(inputPath);
+        String host = "10.0.50.100";
+        int port = 7777;
+        DataStream<String> inputDataStream = env.socketTextStream(host, port);
         DataStream<Tuple2<String, Integer>> wordCountDataStream = inputDataStream
                 .flatMap(new WordCount.MyFlatMapper())
                 .keyBy(0)
